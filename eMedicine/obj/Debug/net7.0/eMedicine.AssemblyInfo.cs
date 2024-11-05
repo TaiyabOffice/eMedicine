7cba4d2081ec3904adf140a39b59776272eaefc6
tariqulshuvon <tariqulshuvon@gmail.com>
2024-11-05 16:37:24 +0600

    redundant data

--- eMedicine/obj/Debug/net7.0/eMedicine.AssemblyInfo.cs
+++ eMedicine/obj/Debug/net7.0/eMedicine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eMedicine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1923c2694f6d7ddd8087d2618122028144291f14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a27ff6bdc93ae0895383ac32ce5ce510f1d9c0c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("eMedicine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eMedicine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

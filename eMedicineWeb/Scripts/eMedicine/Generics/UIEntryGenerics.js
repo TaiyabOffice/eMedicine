b83054002c68edd350b0fa050de4bc20921ed453
taiyabhub <taiyab.hub@gmail.com>
2024-11-11 23:35:44 +0600

    add Modal

--- eMedicineWeb/Scripts/eMedicine/Generics/UIEntryGenerics.js
+++ eMedicineWeb/Scripts/eMedicine/Generics/UIEntryGenerics.js
@@ -1,17 +1,17 @@
 ﻿let rowId = "";
 $(document).ready(function () {
-  
+
     $(".select2").select2();
     $("#btnSave").show();
-    $("#btnUpdate").hide();
+    $("#btnUpdate").hide();  
     jQuery.ajax({
         url: "/Common/GetCurrentDate",
         type: "POST",
         success: function (result) {
             $("#hdnDateToday").datepicker({ format: "dd-M-yyyy", autoclose: true });
-            $("#hdnDateToday").datepicker('setDate', new Date(result));            
+            $("#hdnDateToday").datepicker('setDate', new Date(result));
         }
-    });    
+    });
     GenericsHelper.BuildTbl("");
     GenericsHelper.GetAllGenerics();
 
@@ -41,16 +41,15 @@ var GenericsHelper = {
             type: "POST",
             url: "/Common/GenerateCombo",
             data: json,
-            success: function (data)
-        {
-            if (data.data.length == 1) {
-                $.each(data.data, function (key, item) {
+            success: function (data) {
+                if (data.data.length == 1) {
+                    $.each(data.data, function (key, item) {
                         objcmb.append($("<option></option>").attr("value", item.Id).text(item.Name));
                     });
                 }
                 else {
                     objcmb.append($("<option></option>").attr("value", "0").text("-Select-"));
-                $.each(data.data, function (key, item) {
+                    $.each(data.data, function (key, item) {
                         objcmb.append($("<option></option>").attr("value", item.Id).text(item.Name));
                     });
 
@@ -72,15 +71,15 @@ var GenericsHelper = {
                 { data: 'GenericsDescription' },
                 { data: 'Indications' },
                 { data: 'Contraindications' },
-                { data: 'TherapeuticClass' },                
-                { data: 'SideEffects' },                
+                { data: 'TherapeuticClass' },
+                { data: 'SideEffects' },
                 { data: 'Precautions' },
                 { data: 'Interactions' },
                 { data: 'IsActive' },
                 {
                     data: null,
                     render: function (data, type, row) {
-                        return '<button id="btnEdit" name="btnEdit" type="button" title="Edit" style="margin-right:2px; width:20px; height:20px; padding:0px;" onclick="GenericsHelper.GetGenericsID(\'' + row.GenericsId + '\')" class="btn btn-sm btn-warning"> <i class="fa fa-pencil" style="font-size:15px; padding:0px;"></i></button>';
+                        return '<button id="btnEdit" name="btnEdit" type="button" title="Edit" style="margin-right:2px; width:20px; height:20px; padding:0px;" onclick="GenericsHelper.GetGenericsID(\'' + row.GenericsId + '\')" class="btn btn-sm btn-warning"> <i class="fa fa-pencil" style="font-size:15px; padding:0px;"></i></button><button id="btnDetails" name="btnDetails" type="button" title="Details" style="margin-right:2px; width:20px; height:20px; padding:0px;" onclick="GenericsHelper.GetDetailsByGenericsID(\'' + row.GenericsId + '\')" class="btn btn-sm btn-warning"> <i class="fa fa-eye" style="font-size:15px; padding:0px;"></i></button>';
                     }
                 }
             ],
@@ -89,7 +88,7 @@ var GenericsHelper = {
                     "targets": [0],
                     "width": "2%",
                     render: function (data, type, row, meta) { return meta.row + meta.settings._iDisplayStart + 1; },
-                },                
+                },
                 { "className": "dt-center", "targets": [] },
                 { "className": "dt-left", "targets": [] },
 
@@ -108,7 +107,7 @@ var GenericsHelper = {
             GenericsDescriptionBN: $('#txtDescriptionBN').val(),
             Indications: $('#txtIndications').val(),
             IndicationsBN: $('#txtIndicationsBN').val(),
-            Contraindications: $('#txtContraindications').val(),            
+            Contraindications: $('#txtContraindications').val(),
             ContraindicationsBN: $('#txtContraindicationsBN').val(),
             TherapeuticClass: $('#txtTherapeuticClass').val(),
             TherapeuticClassBN: $('#txtTherapeuticClassBN').val(),
@@ -123,7 +122,7 @@ var GenericsHelper = {
             CreatedDate: $('#hdnDateToday').val(),
             UpdatedBy: $('#hdnUserId').val(),
             UpdatedDate: $('#hdnDateToday').val()
-        };       
+        };
         $.ajax({
             url: '/Generics/CreateGenerics', // Your controller action
             type: 'POST',
@@ -142,8 +141,7 @@ var GenericsHelper = {
                         timer: 2000
                     });
                     GenericsHelper.GetAllGenerics()
-                } else
-                {
+                } else {
                     swal({
                         title: "Sorry!",
                         text: "Saved Failde!",
@@ -152,7 +150,7 @@ var GenericsHelper = {
                         //timer: 2000
                     });
                 }
-                
+
             },
             error: function (xhr, status, error) {
                 // Handle errors
@@ -191,7 +189,7 @@ var GenericsHelper = {
             CreatedDate: $('#hdnDateToday').val(),
             UpdatedBy: $('#hdnUserId').val(),
             UpdatedDate: $('#hdnDateToday').val()
-        };         
+        };
         $.ajax({
             url: '/Generics/UpdateGenericsById', // Your controller action
             type: 'POST',
@@ -209,7 +207,7 @@ var GenericsHelper = {
                 });
                 GenericsHelper.GetAllGenerics();
             },
-            error: function (xhr, status, error) { 
+            error: function (xhr, status, error) {
                 swal({
                     title: "Sorry!",
                     text: "Error retrieving Generics.!" + error,
@@ -226,7 +224,7 @@ var GenericsHelper = {
             url: serviceUrl,
             type: "POST",
             success: function (result) {
-                if (result.success) {                 
+                if (result.success) {
                     GenericsHelper.BuildTbl(result.data);
                 } else {
                     swal({
@@ -261,28 +259,27 @@ var GenericsHelper = {
             data: jsonParam,
             success: function (response) {
                 //console.log(response.data);
-                if (response.Success)
-                {
-                    var Generics = response.data;                    
-                   
+                if (response.Success) {
+                    var Generics = response.data;
+
                     $('#txtGenericsId').val(Generics.GenericsId);
                     $('#txtName').val(Generics.GenericsName);
                     $('#txtNameBN').val(Generics.GenericsNameBN);
                     $('#txtDescription').val(Generics.GenericsDescription);
-                    $('#txtDescriptionBN').val(Generics.GenericsDescriptionBN);               
-                    $('#txtIndications').val(Generics.Indications);                                    
-                    $('#txtIndicationsBN').val(Generics.IndicationsBN);                                    
-                    $('#txtContraindications').val(Generics.Contraindications);                                    
-                    $('#txtContraindicationsBN').val(Generics.ContraindicationsBN);                                    
-                    $('#txtTherapeuticClass').val(Generics.TherapeuticClass);                                    
-                    $('#txtTherapeuticClassBN').val(Generics.TherapeuticClassBN);                                    
-                    $('#txtSideEffects').val(Generics.SideEffects);                                    
-                    $('#txtSideEffectsBN').val(Generics.SideEffectsBN);                                    
-                    $('#txtPrecautions').val(Generics.Precautions);                                    
-                    $('#txtPrecautionsBN').val(Generics.PrecautionsBN);                                    
-                    $('#txtInteractions').val(Generics.Interactions);                                    
+                    $('#txtDescriptionBN').val(Generics.GenericsDescriptionBN);
+                    $('#txtIndications').val(Generics.Indications);
+                    $('#txtIndicationsBN').val(Generics.IndicationsBN);
+                    $('#txtContraindications').val(Generics.Contraindications);
+                    $('#txtContraindicationsBN').val(Generics.ContraindicationsBN);
+                    $('#txtTherapeuticClass').val(Generics.TherapeuticClass);
+                    $('#txtTherapeuticClassBN').val(Generics.TherapeuticClassBN);
+                    $('#txtSideEffects').val(Generics.SideEffects);
+                    $('#txtSideEffectsBN').val(Generics.SideEffectsBN);
+                    $('#txtPrecautions').val(Generics.Precautions);
+                    $('#txtPrecautionsBN').val(Generics.PrecautionsBN);
+                    $('#txtInteractions').val(Generics.Interactions);
                     $('#txtInteractionsBN').val(Generics.InteractionsBN);
-                    $('#CmbIsActive').val(Generics.IsActive);                                    
+                    $('#CmbIsActive').val(Generics.IsActive);
                 } else {
                     swal({
                         title: "Sorry!",
@@ -303,7 +300,74 @@ var GenericsHelper = {
                 });
             }
         });
-    },  
+    },
+
+    GetDetailsByGenericsID: function (GenericsId) {
+
+        var jsonParam = { GenericsId: GenericsId };
+        var serviceUrl = "/Generics/GetGenericsById";
+
+        jQuery.ajax({
+            url: serviceUrl,
+            type: "POST",
+            data: jsonParam,
+            success: function (response) {
+                //console.log(response.data);
+                if (response.Success) {
+                    var Generics = response.data;
+                    GenericsHelper.clrMdl();
+                    $('#mdlTitle').html("Generics Details for: " + Generics.GenericsId + " - " + Generics.GenericsName + " - " + Generics.GenericsNameBN);                    
+                    $('#MdlName').html("Name: " + Generics.GenericsName);
+                    $('#MdlNameBN').html("জেনেরিক নাম: " + Generics.GenericsNameBN);
+                    $('#MdlDescription').html("Description: " + Generics.GenericsDescription);
+                    $('#MdlDescriptionBN').html("বর্ণনা: " + Generics.GenericsDescriptionBN);
+                    $('#MdlIndications').html("Indications: " + Generics.Indications);
+                    $('#MdlIndicationsBN').html("ইঙ্গিত: " + Generics.IndicationsBN);
+                    $('#MdlContraindications').html("Contraindications: " + Generics.Contraindications);
+                    $('#MdlContraindicationsBN').html("বিপরীত: " + Generics.ContraindicationsBN);
+                    $('#MdlTherapeuticClass').html("Therapeutic-Class: " + Generics.TherapeuticClass);
+                    $('#MdlTherapeuticClassBN').html("থেরাপিউটিক ক্লাস: " + Generics.TherapeuticClassBN);
+                    $('#MdlSideEffects').html("Side-Effects: " + Generics.SideEffects);
+                    $('#MdlSideEffectsBN').html("পার্শ্ব প্রতিক্রিয়া: " + Generics.SideEffectsBN);
+                    $('#MdlPrecautions').html("Precautions: " + Generics.Precautions);
+                    $('#MdlPrecautionsBN').html("সতর্কতা: " + Generics.PrecautionsBN);
+                    $('#MdlInteractions').html("Interactions: " + Generics.Interactions);
+                    $('#MdlInteractionsBN').html("মিথষ্ক্রিয়া: " +Generics.InteractionsBN);
+                    $('#MdlCmbIsActive').html("IsActive: " + Generics.IsActive);
+                    $("#modal-default").modal("show");
+                }
+                else {
+                    swal({
+                        title: "Sorry!",
+                        text: "No Generics data found.!",
+                        type: "error",
+                        closeOnConfirm: false,
+                        //timer: 2000
+                    });
+                }
+            }
+        });
+    },
+    clrMdl: function (o)
+    {
+        $('#MdlGenericsId').html("");
+        $('#MdlName').html("");
+        $('#MdlNameBN').html("");
+        $('#MdlDescription').html("");
+        $('#MdlDescriptionBN').html("");
+        $('#MdlIndications').html("");
+        $('#MdlIndicationsBN').html("");
+        $('#MdlContraindications').html("");
+        $('#MdlContraindicationsBN').html("");
+        $('#MdlTherapeuticClass').html("");
+        $('#MdlTherapeuticClassBN').html("");
+        $('#MdlSideEffects').html("");
+        $('#MdlSideEffectsBN').html("");
+        $('#MdlPrecautions').html("");
+        $('#MdlPrecautionsBN').html("");
+        $('#MdlInteractions').html("");
+        $('#MdlInteractionsBN').html("");      
+    },
     getSelectionStart: function (o) {
         if (o.createTextRange) {
             var r = document.selection.createRange().duplicate();

5b4d39ba6868852b4710b1267ab3ddec6f02ce7b
tariqulshuvon <tariqulshuvon@gmail.com>
2024-11-16 22:55:02 +0600

    Registration  page added

---
+++ eMedicineWeb/Controllers/RegistrationController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eMedicineWeb.Controllers
+{
+    [AllowAnonymous]
+    public class RegistrationController : Controller
+    {
+        DataSet ds = new DataSet();
+        public ActionResult UserRegister()
+        {
+            return View();
+        }
+
+
+        public JsonResult SaveRegistration()
+        {
+
+            if (Session["UserID"] == null)
+            {
+                return new JsonResult { Data = new { status = "Logout" } };
+            }
+            else
+            {
+                bool status = false;
+         
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    status = true;
+                }
+                else
+                {
+                    status = false;
+                }
+                return new JsonResult { Data = new { status = status } };
+            }
+        }
+
+
+    }
+}
---
+++ eMedicineWeb/Scripts/Registration/Register.js
@@ -0,0 +1,145 @@
+﻿$(document).ready(function () {
+    RegistrationHelper.ValidateRegistration();
+
+});
+
+$("#btnRegister").click(function () {
+    if ($("#validateRegistration").valid()) {
+        $("body").addClass("loading");
+        RegistrationHelper.RegisterData();
+    }
+});
+
+$("#txtEmail, #txtPassword, #txtConfirmPassword").on("keydown", function (e) {
+    if (e.keyCode == 13) {
+        if ($("#validateRegistration").valid()) {
+            $("#btnRegister").click();
+        }
+    }
+});
+
+var RegistrationHelper = {
+    ShowPassword: function (fieldId, eyeIcon) {
+        var field = document.getElementById(fieldId);
+        var icon = eyeIcon;
+        if (field.type === "password") {
+            field.type = "text";
+            icon.className = "fa fa-eye field-icon2";
+        } else {
+            field.type = "password";
+            icon.className = "fa fa-eye-slash field-icon2";
+        }
+    },
+
+    ValidateRegistration: function () {
+        //$.validator.addMethod("passwordMatch", function (value, element) {
+        //    return value === $("#txtPassword").val();
+        //}, "Passwords do not match");
+
+        $("#registrationForm").validate({
+            rules: {
+                userId: "required",
+                username: "required",
+                phone: {
+                    required: true,
+                    digits: true,
+                    minlength: 10,
+                    maxlength: 15
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                    minlength: 6
+                },
+                confirmPassword: {
+                    required: true,
+                    equalTo: "#password"
+                }
+            },
+            messages: {
+                userId: "User ID is required",
+                username: "Username is required",
+                phone: {
+                    required: "Phone Number is required",
+                    digits: "Please enter a valid phone number",
+                    minlength: "Phone number must be at least 10 digits",
+                    maxlength: "Phone number must not exceed 15 digits"
+                },
+                email: {
+                    required: "Email Address is required",
+                    email: "Please enter a valid email address"
+                },
+                password: {
+                    required: "Password is required",
+                    minlength: "Password must be at least 6 characters"
+                },
+                confirmPassword: {
+                    required: "Please confirm your password",
+                    equalTo: "Password did not match"
+                }
+            },
+            errorPlacement: function (label, element) {
+                label.addClass('error');
+                element.parent().append(label);
+            }
+        });
+    },
+
+    RegisterData: function () {
+        var obj = new Object();
+
+        obj.DESC1 = $("#txtUserId").val();
+        obj.DESC2 = $("#txtUserName").val();
+        obj.DESC3 = $("#txtPhone").val();
+        obj.DESC4 = $("#txtEmail").val();
+        obj.DESC5 = $("#txtPassword").val();
+
+
+        var objDetails = JSON.stringify(obj);
+
+        var jsonParam = "objDetails:" + objDetails;
+
+        var serviceUrl = "/Registration/SaveRegistration";
+        jQuery.ajax({
+            url: serviceUrl,
+            type: "POST",
+            data: "{" + jsonParam + "}",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function (data) {
+                if (data.status) {
+
+                    swal({
+                        title: "Congratulation!!",
+                        text: "Save Successfully",
+                        type: "success",
+                        closeOnConfirm: false,
+
+                    });
+                    location.reload();
+
+                } else {
+                    swal({
+                        title: "Sorry!",
+                        text: "Failed to save!",
+                        type: "error",
+                        closeOnConfirm: false,
+                    });
+                }
+            },
+            error: function (data) {
+                swal({
+                    title: "Sorry!",
+                    text: "Something Went Wrong !!! \n" + data.statusText,
+                    type: "error",
+                    closeOnConfirm: false
+                });
+            }
+
+        });
+    }
+};
+

b03bf073296c1ae8d37697d5ecf4c442e24f4cfd
TaiyabOffice <Taiyaboffice@gmail.com>
2025-07-09 13:09:41 +0600

    text

--- eMedicine/obj/Debug/net7.0/eMedicine.AssemblyInfo.cs
+++ eMedicine/obj/Debug/net7.0/eMedicine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eMedicine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c043cae92194d5775ed6c9839270799e621edef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+769a18638dbaf1bb1eed7552e0677223215de4b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("eMedicine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eMedicine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
